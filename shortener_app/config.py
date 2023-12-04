e4e8f5bc65aca0f43214524a981cecb55fbb1e57
kaebmoo <kaebmoo@yahoo.com>
2023-12-04 16:51:09 +0700

    Update config.py

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -7,9 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
 
     env_name: str = "Local"
-
     base_url: str = "http://localhost:8000"
-
     db_url: str = "sqlite:///./shortener.db"
     model_config = SettingsConfigDict(env_file=".env")
 
@@ -18,7 +16,6 @@ class Settings(BaseSettings):
 def get_settings() -> Settings:
 
     settings = Settings()
-
     print(f"Loading settings for: {settings.env_name}")
 
     return settings
